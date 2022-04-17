3bf966359931a3e921c6725f4ecb271466015676
lukman1234 <olamidelukman3@gmail.com>
2022-04-17 13:07:08 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function view(num) {
+    document.getElementById('result').value+=num
+}
+
+function compute() {
+    let x = document.getElementById('result').value
+    let y = eval(x)
+    document.getElementById('result').value = y
+}
+
+function clr() {
+    document.getElementById ('result').value = ''
+}
